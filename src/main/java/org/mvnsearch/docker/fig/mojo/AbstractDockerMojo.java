9186abb177fc804124adf0e8d3fa3f99d52a5c43
linux_china <linux_china@hotmail.com>
2014-11-27 22:28:40 +0800

    minor update

--- src/main/java/org/mvnsearch/docker/fig/mojo/AbstractDockerMojo.java
+++ src/main/java/org/mvnsearch/docker/fig/mojo/AbstractDockerMojo.java
@@ -15,10 +15,8 @@ import java.util.Properties;
 public abstract class AbstractDockerMojo extends AbstractMojo {
 
     /**
-     * The project name, e.g. -Dfig.project.name=demo
+     * The docker, e.g. -Dfig.docker.host=tcp://192.168.59.103:2375
      */
-    @Parameter(property = "fig.project.name")
-    private String projectName;
     @Parameter(property = "fig.docker.host")
     private String dockerHost;
 
@@ -34,12 +32,8 @@ public abstract class AbstractDockerMojo extends AbstractMojo {
         if (dockerHost != null && !dockerHost.isEmpty()) {
             properties.put("DOCKER_HOST", dockerHost);
         }
-        if (projectName != null && !projectName.isEmpty()) {
-            properties.put("fig.project.name", projectName);
-        }
         try {
-            //getLog().info("Docker version " + );
-            doExecute(new FigOrchestrator(projDir(), properties));
+            doExecute(new FigOrchestrator(project.getBasedir(), properties));
         } catch (Exception e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
@@ -66,14 +60,6 @@ public abstract class AbstractDockerMojo extends AbstractMojo {
         }
         p.putAll(project.getProperties());
         return p;
-    }
-
-    private File workDir() {
-        return new File(project.getBuild().getDirectory(), "docker");
-    }
-
-    private File projDir() {
-        return project.getBasedir();
     }
 
     protected abstract void doExecute(FigOrchestrator orchestrator) throws Exception;
